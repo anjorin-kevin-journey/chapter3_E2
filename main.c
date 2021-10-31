7aa1b925a8daf3588c22128b393faa2af47a55f4
Kevin <kevin,anjorin@gmx.at>
2021-10-31 21:59:50 +0100

    Starting to solve the exercise from CProgramming A modern approach Second Edition ISBN-10: 0-393-97950-4

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    // Collect data
+    int item_number,dd,mm,yyyy;
+    float unit_price;
+    printf("Enter item number : ");
+    scanf("%d",&item_number);
+    printf("Enter unit price: ");
+    scanf("%f",&unit_price);
+    printf("Enter purchase date (mm/dd/yyyy) : ");
+    scanf("%d/%d/%d",&mm,&dd,&yyyy);
+
+    // Print everything
+    printf("Item\t\tUnit\t\tPurchase\n"
+           "\t\tPrice\t\tDate\n"
+           " %-d\t\t$%6.2f\t\t%-d/%-d/%-d\n",item_number,unit_price,mm,dd,yyyy);
+
     return 0;
 }
